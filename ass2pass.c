3107b730634004c79af7d79df91d650706185e17
Divya Ann Kurien <divya.kurien@gmail.com>
2019-10-17 07:57:06 +0530

    Assembler 1st pass added

---
+++ ass2pass.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+	char label[10],opcode[10],operand[10];
+	file *f1,*f2,*f3;
+	f1 = fopen("input.txt");
+	f2 = fopen("symtab.txt");
+	f3 = fopen("output.txt");
+	fscan(f1,"%s%s%s",label,opcode,operand);
+	
+
+
