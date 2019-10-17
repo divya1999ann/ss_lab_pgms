5f1ec9d6fbad742c0b7b96d8ddc8c8dd22873bce
Divya Ann Kurien <divya.kurien@gmail.com>
2019-10-17 08:04:40 +0530

    Input file made changes

--- ass2pass.c
+++ ass2pass.c
@@ -4,11 +4,13 @@
 void main()
 {
 	char label[10],opcode[10],operand[10];
-	file *f1,*f2,*f3;
+	file *f1,*f2,*f3,*f4;
 	f1 = fopen("input.txt");
+	f2 = fopen("optab.txt");
 	f2 = fopen("symtab.txt");
 	f3 = fopen("output.txt");
 	fscan(f1,"%s%s%s",label,opcode,operand);
+	if(
 	
 
 
